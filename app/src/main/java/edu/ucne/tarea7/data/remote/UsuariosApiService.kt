fa5812111cc1aa1281b92c0375375c0949cd9cb3
Danielvi <danielvilopez@gmail.com>
2025-11-18 12:28:05 -0400

    Crea la interfaz de la Api que contiene las funciones de la Api

---
+++ app/src/main/java/edu/ucne/tarea7/data/remote/UsuariosApiService.kt
@@ -0,0 +1,25 @@
+package edu.ucne.tarea7.data.remote
+
+import edu.ucne.tarea7.data.remote.dto.UsuariosDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface UsuariosApiService {
+    @GET("api/Usuarios")
+    suspend fun getUsuarios(): Response<List<UsuariosDto>>
+
+    @GET("api/Usuarios/{id}")
+    suspend fun getUsuario(@Path("id") id: Int): Response<UsuariosDto?>
+
+    @PUT("api/Usuarios/{id}")
+    suspend fun putUsuario(@Path("id") id: Int, @Body usuario: UsuariosDto): Response<Unit>
+
+    @POST("api/Usuarios")
+    suspend fun postUsuario(@Body usuario: UsuariosDto): Response<UsuariosDto?>
+}
+
+
